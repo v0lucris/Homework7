0cac8d5f6c2d72400d38e3db82ad4a7d48ba46c1
Tati <tanushka-p.94@mail.ru>
2022-09-17 08:23:47 +0300

    save1 task1

---
+++ Task1/Program.cs
@@ -0,0 +1,102 @@
+﻿// Найти произведение 2-х матриц
+void Fillmass1(int[,]mass1)
+{
+    for (int i = 0; i < mass1.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass1.GetLength(1); j++)
+        {
+            mass1[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+void Printmass1(int[,]mass1)
+{
+    for (int i = 0; i < mass1.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < mass1.GetLength(1); j++)
+        {
+            Console.Write($"{mass1[i, j]} ");
+        }
+        Console.Write("]");
+    Console.WriteLine(" ");
+    }
+}
+
+void Fillmass2(int[,]mass2)
+{
+    for (int m = 0; m < mass2.GetLength(0); m++)
+    {
+        for (int n = 0; n < mass2.GetLength(1); n++)
+        {
+            mass2[m,n] = new Random().Next(1,10);
+        }
+    }
+}
+void Printmass2(int[,]mass2)
+{
+    for (int m = 0; m < mass2.GetLength(0); m++)
+    {
+        Console.Write("[");
+        for (int n = 0; n < mass2.GetLength(1); n++)
+        {
+            Console.Write($"{mass2[m, n]} ");
+        }
+        Console.Write("]");
+    Console.WriteLine(" ");
+    }
+}
+
+Console.WriteLine("i");
+int i = int.Parse(Console.ReadLine()); 
+Console.WriteLine("j");
+int j = int.Parse(Console.ReadLine());
+Console.WriteLine("n");
+int n = int.Parse(Console.ReadLine());
+int[,] mass1 = new int [i,j]; 
+int[,] mass2 = new int [j,n];
+int[,] mass3 = new int [i,n];
+void Multiplication(int[,]mass1, int[,]mass2, int[,]mass3)
+{
+    for (int i = 0; i < mass1.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass1.GetLength(1); j++)
+        {
+            for (int n = 0; n < mass2.GetLength(1); n++)
+            {
+               mass3[i,n] +=mass1[i,j]*mass2[j,n]; 
+            }
+                                   
+        }
+    }
+
+}
+void PrintMultiplication(int [,] mass3)
+{
+    for (int s = 0; s < mass3.GetLength(0); s++)
+    {
+        Console.Write("[");
+        for (int k = 0; k < mass3.GetLength(1); k++)
+        {
+             Console.Write($"{mass3[s, k]} ");
+        }
+        Console.Write("]");
+        Console.WriteLine();
+    }     
+}
+
+
+
+Console.WriteLine();
+Fillmass1(mass1);
+Printmass1(mass1);
+Console.WriteLine();
+Console.WriteLine();
+Fillmass2(mass2);
+Printmass2(mass2);
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Произведение матриц равно");
+Multiplication(mass1, mass2, mass3);
+PrintMultiplication(mass3);
+Console.WriteLine();
---
+++ Task2/Program.cs
@@ -0,0 +1,81 @@
+﻿// В двухмерном массиве целых чисел удалить строку и столбец на пересечении которых расположен наименьший элемент
+
+
+void Fillarray(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            mtrx[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void Printarray(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            Console.Write($"{mtrx[i, j]} ");
+        }
+        Console.Write("]");
+    Console.WriteLine(" ");
+    }
+}
+
+Console.WriteLine("m");
+int m = int.Parse(Console.ReadLine()); 
+Console.WriteLine("n");
+int n = int.Parse(Console.ReadLine());
+int[,] mtrx = new int [m,n];
+
+
+Fillarray(mtrx);
+Console.Write(" ");
+Printarray( mtrx);
+Console.Write(" ");
+
+int x = 0;
+int y = 0;
+int min = mtrx[0, 0];
+
+for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            if(mtrx[i,j]< min ) 
+            {
+                min = mtrx[i,j];
+                x = i;
+                y = j;
+                
+            } 
+                       
+        }
+    }
+Console.WriteLine($"Наименьший элемент : {min} ");
+
+Console.WriteLine($"Your new array ");
+for (int i = 0; i < mtrx.GetLength(0); i++)
+   {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            {
+            if ((i == x) || (j == y))
+            
+                continue;
+            else
+                Console.Write(mtrx[i, j] + " ");
+            }
+        }  
+        Console.WriteLine();          
+    }   
+   
+
+
+
+
+
